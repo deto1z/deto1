cda55eb40e0eabd60cffe848c408c9fa66fee714
deto1z <Anthony.flores2021@gmail.com>
2025-06-12 22:03:15 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
-import '@/index.css';
+import App from './App.jsx'; // Importa el componente principal de tu aplicación
+import './index.css'; // Importa tus estilos CSS globales (usado por Tailwind CSS)
 
+// Obtiene el elemento raíz del DOM donde se montará la aplicación
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Renderiza el componente principal de tu aplicación (App) */}
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
